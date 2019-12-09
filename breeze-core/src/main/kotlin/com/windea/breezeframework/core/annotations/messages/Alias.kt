4f4f87b87f42122e2854232810f4029c5336665c
DragonKnightOfBreeze <2944262241@qq.com>
2019-12-09 14:02:39 +0800

    清理注解。

--- breeze-core/src/main/kotlin/com/windea/breezeframework/core/annotations/messages/Alias.kt
+++ breeze-core/src/main/kotlin/com/windea/breezeframework/core/annotations/messages/Alias.kt
@@ -1,12 +1,11 @@
 package com.windea.breezeframework.core.annotations.messages
 
 import kotlin.annotation.AnnotationTarget.*
-import kotlin.reflect.*
 
-/**引用。*/
+/**别名。*/
 @MustBeDocumented
 @Target(CLASS, PROPERTY, FIELD)
-annotation class Reference(
-	/**引用一览。*/
-	vararg val value: KClass<*>
+annotation class Alias(
+	/**别名一览。*/
+	vararg val value: String
 )
