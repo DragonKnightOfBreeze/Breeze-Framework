6579071e99f3e367ce30d20a39486f61f2f8d9e3
DragonKnightOfBreeze <2944262241@qq.com>
2020-03-12 14:07:54 +0800

    完善代码。
    Signed-off-by: DragonKnightOfBreeze <2944262241@qq.com>

--- breeze-core/src/main/kotlin/com/windea/breezeframework/core/domain/DataEntity.kt
+++ breeze-core/src/main/kotlin/com/windea/breezeframework/core/domain/DataEntity.kt
@@ -1,9 +1,11 @@
 package com.windea.breezeframework.core.domain
 
+import java.io.*
+
 /**
  * 数据实体。
  *
  * 数据实体用于存储一系列的数据，并且保证最小的可见性。
- * 数据实体应当是一个数据类，可以通过其构造方法创建，可以通过其构建器的构建方法或代码块创建。
+ * 数据实体应当是一个数据类，可以通过其构造方法创建，也可以通过其构建器的构建方法创建。
  */
-interface DataEntity
+interface DataEntity : Serializable
