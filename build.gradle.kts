54ae6dd2690e5eefa3376392f1e556e6c1c7be2c
DragonKnigtOfBreeze <dk_breeze@qq.com>
2021-08-02 17:34:29 +0800

    更新版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 val groupName = "icu.windea.breezeframework"
-val versionName = "3.0.0"
+val versionName = "3.0.1"
 val packagePrefix = "icu.windea.breezeframework"
 val compilerArgs = listOf(
 	"-Xinline-classes",
