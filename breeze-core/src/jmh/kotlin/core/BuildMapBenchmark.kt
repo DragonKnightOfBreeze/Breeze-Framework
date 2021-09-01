da1959aabc277db0dfa6066d8a16ab23c2fe9f49
DragonKnigtOfBreeze <dk_breeze@qq.com>
2021-09-01 19:51:11 +0800

    更新中

--- breeze-core/src/jmh/kotlin/core/BuildMapBenchmark.kt
+++ breeze-core/src/jmh/kotlin/core/BuildMapBenchmark.kt
@@ -15,16 +15,16 @@ import java.util.concurrent.*
 @Measurement(iterations = 5, time = 1)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 class BuildMapBenchmark {
-	//Benchmark                Mode  Cnt    Score     Error  Units
-	//BuildMapBenchmark.test1  avgt    5   43.385 ±  12.298  ns/op    HashMap().apply{ ... }
-	//BuildMapBenchmark.test2  avgt    5   72.233 ±   9.938  ns/op    LinkedHashMap().apply{ ... }
-	//BuildMapBenchmark.test3  avgt    5   52.149 ±   8.330  ns/op    Collections.unmodifiableMap(LinkedHashMap().apply{ ... })
-	//BuildMapBenchmark.test4  avgt    5  189.099 ± 111.274  ns/op    ImmutableMap.copyOf(LinkedHashMap().apply{ ... })
-	//BuildMapBenchmark.test5  avgt    5   65.384 ±  13.226  ns/op    mapOf(...)
-	//BuildMapBenchmark.test6  avgt    5   88.053 ±  37.805  ns/op    mutableMapOf(...)
-	//BuildMapBenchmark.test7  avgt    5   51.561 ±  13.756  ns/op    buildMap{ ... }
-	//BuildMapBenchmark.test8  avgt    5   44.437 ±   7.382  ns/op    ImmutableMap.of(...)
-	//BuildMapBenchmark.test9  avgt    5   97.129 ±  44.223  ns/op    ImmutableMap.copyOf(pairs)
+	//Benchmark                Mode  Cnt    Score    Error  Units
+	//BuildMapBenchmark.test1  avgt    5   44.707 ±  1.253  ns/op    HashMap().apply{ ... }
+	//BuildMapBenchmark.test2  avgt    5   70.900 ± 12.724  ns/op    LinkedHashMap().apply{ ... }
+	//BuildMapBenchmark.test3  avgt    5   73.258 ±  7.930  ns/op    Collections.unmodifiableMap(LinkedHashMap().apply{ ... })
+	//BuildMapBenchmark.test4  avgt    5  135.810 ±  6.153  ns/op    ImmutableMap.copyOf(LinkedHashMap().apply{ ... })
+	//BuildMapBenchmark.test5  avgt    5   71.563 ±  7.220  ns/op    mapOf(...)
+	//BuildMapBenchmark.test6  avgt    5   79.752 ±  7.877  ns/op    mutableMapOf(...)
+	//BuildMapBenchmark.test7  avgt    5   48.432 ±  1.484  ns/op    buildMap{ ... }
+	//BuildMapBenchmark.test8  avgt    5   49.905 ±  1.627  ns/op    ImmutableMap.of(...)
+	//BuildMapBenchmark.test9  avgt    5   59.035 ±  3.054  ns/op    ImmutableMap.copyOf(pairs)
 
 	@Benchmark fun test1() = HashMap<String, String>().apply { put("1", "a");put("2", "b");put("3", "c") }
 
